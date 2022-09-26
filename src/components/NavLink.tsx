fd6ce80ca87f2ed46c38c57d1f6c1378a7412e4e
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-26 16:58:36 -0400

    refactor

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -9,21 +9,17 @@ type Props = {
 export const NavLink: React.FC<Props> = ({ children, to }) => {
 	const router = useRouter();
 
-	let path = router.pathname.replace('/_sites/[site]', '');
-	if (path === '') {
-		path = '/';
-	}
+	let path = router.pathname;
 
-	const active = path === to;
+	const activeStyles =
+		path === to
+			? 'scale-110 text-neutral-700'
+			: 'text-neutral-500 hover:scale-110 hover:text-neutral-700';
 
 	return (
 		<Link
 			href={to}
-			className={`font-heading text-sm transform origin-left transition uppercase tracking-wide ${
-				active
-					? 'scale-110 text-neutral-700'
-					: 'text-neutral-500 hover:scale-110 hover:text-neutral-700'
-			}`}
+			className={`font-heading text-sm transform origin-left transition uppercase tracking-wide ${activeStyles}`}
 		>
 			{children}
 		</Link>
