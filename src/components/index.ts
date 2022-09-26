120fd31bbf14da89ae2473f594d082d78f4e5910
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-26 19:19:15 -0400

    moved button, side nav, and navlink to naviagtion folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,14 @@
-export { Button } from './Button';
 export { CTA } from './CTA';
 export { Footer } from './Footer';
 export { Hero } from './Hero';
 export { Centered } from './layout/Centered';
 export { Container } from './layout/Container';
 export { Meet } from './Meet';
-export { NavLink } from './NavLink';
+export { Button } from './navigation/Button';
+export { NavLink } from './navigation/NavLink';
+export { SideNav } from './navigation/SideNav';
 export { Quote } from './Quote';
 export { Showcase } from './Showcase';
-export { SideNav } from './SideNav';
 export { SocialIcon } from './SocialIcon';
 export { Socials } from './Socials';
 export { Testimonials } from './Testimonials';
