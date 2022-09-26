92dcd0c34b519e326aaae17b70695051911760a1
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-26 12:20:54 -0400

    fixed border on cta

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button: React.FC<Props> = ({ children, to }) => {
 	return (
 		<Link
 			href={to}
-			className="text-sm text-neutral-500 hover:text-black transition hover:border-black font-heading border py-2 px-4 border-neutral-300 inline-flex"
+			className="text-sm text-neutral-500 hover:text-black transition hover:border-black font-heading border py-2 px-4 border-neutral-200 inline-flex"
 		>
 			{children}
 		</Link>
