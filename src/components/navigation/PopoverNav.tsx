3a2b6d436fd79622f01277989bae7ac93a8374d1
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-26 20:53:02 -0400

    popover nav finished

--- src/components/navigation/PopoverNav.tsx
+++ src/components/navigation/PopoverNav.tsx
@@ -1,25 +1,26 @@
-import { Initials, MenuButton } from 'components';
+import { Initials, MenuButton, NavLink, Socials } from 'components';
 
-import Link from 'next/link';
 import { Popover } from '@headlessui/react';
 
 export const PopoverNav: React.FC = () => {
 	return (
 		<nav className="z-50 flex items-center justify-between p-8 fixed md:sticky top-0 right-0 left-0">
 			<Initials />
-			<Popover className="relative">
+			<Popover>
 				{({ open }) => (
 					<>
 						<MenuButton open={open} />
-						<Popover.Panel className="absolute z-10">
-							<div className="grid grid-cols-2">
-								<Link href="/">Home</Link>
-								<Link href="/portfolio">Portfolio</Link>
-								<Link href="/about">About</Link>
-								<Link href="/testimonials">Testimonials</Link>
-								<Link href="/inquire">Inquire</Link>
-								<Link href="/dianne-boudoir">Dianne Boudoir</Link>
+						<Popover.Panel className="absolute z-10 right-0 left-0 top-0 bg-white p-8 flex flex-col gap-8 shadow-lg">
+							<Initials />
+							<div className="flex flex-col gap-4 items-start">
+								<NavLink to="/">Home</NavLink>
+								<NavLink to="/portfolio">Portfolio</NavLink>
+								<NavLink to="/about">About</NavLink>
+								<NavLink to="/testimonials">Testimonials</NavLink>
+								<NavLink to="/inquire">Inquire</NavLink>
+								<NavLink to="/dianne-boudoir">Dianne Boudoir</NavLink>
 							</div>
+							<Socials />
 						</Popover.Panel>
 					</>
 				)}
