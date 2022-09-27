333979c48aa7bf306914a3aba1a7975819a24cca
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-26 21:15:15 -0400

    more responsive showcase

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -6,10 +6,17 @@ export const Showcase: React.FC = () => {
 	return (
 		<Container>
 			<div className="flex flex-col gap-16">
-				<div className="grid grid-cols-3 gap-8 md:gap-16 items-stretch place-items-center">
+				<div className="grid grid-cols-2 sm:grid-cols-3 gap-8 md:gap-16 items-stretch place-items-center">
 					<Image src="/img/showcase/1.jpg" alt="1" width={300} height={350} />
 					<Image src="/img/showcase/2.jpg" alt="2" width={300} height={350} />
 					<Image src="/img/showcase/3.jpg" alt="3" width={300} height={350} />
+					<Image
+						src="/img/showcase/4.jpg"
+						alt="3"
+						width={300}
+						height={350}
+						className="block sm:hidden"
+					/>
 				</div>
 				<div className="flex flex-col items-center gap-4">
 					<h2 className="font-heading text-neutral-700 uppercase text-2xl tracking-widest">
