7bb2d22d292b2c9651e7ea7315b347fe90474f23
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-26 21:29:47 -0400

    testimonial with real copy

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -10,16 +10,20 @@ export const Testimonials: React.FC = () => {
 
 				<div className="max-w-md flex flex-col gap-4 items-center">
 					<h3 className="text-4xl font-script text-neutral-500 tracking-wider text-center">
-						Perry Jurick
+						Drew + Tracy
 					</h3>
 					<p className="text-neutral-500 font-body text-justify">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-						cupiditate fugit eum quasi nobis, a sapiente voluptas mollitia rerum
-						iste, delectus provident recusandae dicta quae aspernatur earum ea
-						facere maiores. Lorem ipsum dolor sit amet consectetur adipisicing
-						elit. Fuga autem ex odit aliquid ipsa fugit non tempora. Accusamus
-						quaerat, tempora fugiat itaque repellendus aspernatur voluptate quos
-						cupiditate error eum amet?
+						Perry was absolutely amazing to work with. She was responsive,
+						understanding, kind, and so flexible to our time constraints,
+						changes, and weather issues. She was great with guidance and
+						communication. She came early and did her own thing while I was
+						getting ready--taking detail shots and working in the background.
+						She was amazing at redirecting our family and friends for pictures,
+						and she was so good at going with the flow. She was just amazing to
+						work with. Her photo style is light, natural, and all things
+						beautiful, and we will treasure our pictures forever. I cannot say
+						enough great things about this girl. Thanks so much Perry! She will
+						not let you down, brides!
 					</p>
 				</div>
 			</Centered>
