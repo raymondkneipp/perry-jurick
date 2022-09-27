0531ba7dbb257b8c32bbd28a89fb2162d658f967
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-27 15:01:51 -0400

    popover nav fades in

--- src/components/navigation/PopoverNav.tsx
+++ src/components/navigation/PopoverNav.tsx
@@ -1,6 +1,5 @@
 import { Initials, MenuButton, NavLink, Socials } from 'components';
-
-import { Popover } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
 
 export const PopoverNav: React.FC = () => {
 	return (
@@ -10,18 +9,29 @@ export const PopoverNav: React.FC = () => {
 				{({ open }) => (
 					<>
 						<MenuButton open={open} />
-						<Popover.Panel className="absolute z-10 right-0 left-0 top-0 bg-white p-8 flex flex-col gap-8 shadow-lg bg-opacity-50 backdrop-blur-sm">
-							<Initials />
-							<div className="flex flex-col gap-4 items-start">
-								<NavLink to="/">Home</NavLink>
-								<NavLink to="/portfolio">Portfolio</NavLink>
-								<NavLink to="/about">About</NavLink>
-								<NavLink to="/testimonials">Testimonials</NavLink>
-								<NavLink to="/inquire">Inquire</NavLink>
-								<NavLink to="/dianne-boudoir">Dianne Boudoir</NavLink>
-							</div>
-							<Socials />
-						</Popover.Panel>
+						<Transition
+							as="div"
+							className="absolute right-0 left-0 top-0"
+							enter="transition duration-100 ease-out"
+							enterFrom="opacity-0"
+							enterTo="opacity-100"
+							leave="transition duration-75 ease-out"
+							leaveFrom="opacity-100"
+							leaveTo="opacity-0"
+						>
+							<Popover.Panel className="absolute z-10 right-0 left-0 top-0 bg-white p-8 flex flex-col gap-8 shadow-lg bg-opacity-50 backdrop-blur-sm">
+								<Initials />
+								<div className="flex flex-col gap-4 items-start">
+									<NavLink to="/">Home</NavLink>
+									<NavLink to="/portfolio">Portfolio</NavLink>
+									<NavLink to="/about">About</NavLink>
+									<NavLink to="/testimonials">Testimonials</NavLink>
+									<NavLink to="/inquire">Inquire</NavLink>
+									<NavLink to="/dianne-boudoir">Dianne Boudoir</NavLink>
+								</div>
+								<Socials />
+							</Popover.Panel>
+						</Transition>
 					</>
 				)}
 			</Popover>
