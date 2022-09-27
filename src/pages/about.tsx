5d0c0aa8ba660c92dbbd85c3b00f41e2274db9f3
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-27 17:04:52 -0400

    about page created

---
+++ src/pages/about.tsx
@@ -0,0 +1,50 @@
+import {
+	Button,
+	CTA,
+	Container,
+	Meet,
+	Page,
+	PopoverNav,
+	Quote,
+	Spacing,
+	Testimonials,
+} from 'components';
+
+import type { NextPage } from 'next';
+
+const About: NextPage = () => {
+	return (
+		<Page>
+			<PopoverNav />
+			<Spacing>
+				<Quote>
+					There is nothing better than loving and being loved in return.
+				</Quote>
+				<Meet />
+				<Testimonials />
+
+				<Container>
+					<div className="flex flex-col gap-4 items-start">
+						<h1 className="font-heading text-2xl uppercase tracking-widest">
+							Hello World
+						</h1>
+						<p className="font-body text-neutral-500 text-justify">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius unde
+							eum atque odit maiores cum perspiciatis, odio aut fugiat? Maiores,
+							labore aliquam? Nihil deleniti officiis voluptate, magnam tempora
+							odit beatae! Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Tempora ipsa dolore praesentium fuga aperiam tenetur fugit
+							minima, hic tempore eligendi consequatur, corporis excepturi
+							exercitationem vero ea, esse necessitatibus quibusdam magni?
+						</p>
+						<Button to="/about">Read More</Button>
+					</div>
+				</Container>
+
+				<CTA />
+			</Spacing>
+		</Page>
+	);
+};
+
+export default About;
