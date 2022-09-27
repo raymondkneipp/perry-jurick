be96dd00f96cc079586731e0d30fb72143effc2c
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-09-27 18:45:09 -0400

    quote sizing

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,17 +1,13 @@
-import { Container } from 'components';
-
 type Props = {
 	children: string;
 };
 
 export const Quote: React.FC<Props> = ({ children }) => {
 	return (
-		<Container>
-			<div className="py-16 flex items-center justify-center">
-				<q className="font-heading text-2xl text-center text-neutral-500 max-w-2xl uppercase tracking-widest">
-					{children}
-				</q>
-			</div>
-		</Container>
+		<div className="py-16 flex items-center justify-center">
+			<q className="font-heading text-xl text-center text-neutral-500 max-w-2xl uppercase tracking-widest">
+				{children}
+			</q>
+		</div>
 	);
 };
